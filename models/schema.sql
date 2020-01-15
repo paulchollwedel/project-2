a6c1643fc98963068c0880ace7958c8ba6f37dae
Lakshdeep Bajwa <bajwals@live.com>
2020-01-14 21:21:15 -0800

    created db schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,32 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS instaArt;
+CREATE DATABASE instaArt;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE instaArt;
+
+CREATE TABLE artists (
+    id INT NOT NULL AUTO_INCREMENT,
+    firstname VARCHAR(45) NULL,
+    lastname VARCHAR(45) NULL,
+    username VARCHAR(45) NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE artwork (
+    id INT NOT NULL AUTO_INCREMENT,
+    artist_id INT NOT NULL,
+    title VARCHAR(45) NULL,
+    img_url VARCHAR(100) NULL,
+    price INT NULL,
+    in_stock INT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE customers (
+    id INT NOT NULL AUTO_INCREMENT,
+    firstname VARCHAR(45) NULL,
+    lastname VARCHAR(45) NULL,
+    username VARCHAR(45) NULL,
+    credit_card INT NULL,
+    mailing_address VARCHAR(45) NULL,
+    PRIMARY KEY (id)
+);
