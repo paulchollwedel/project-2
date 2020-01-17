19cf53280ab94dcd5b36b8f774076e649c0b3dc7
Lakshdeep Bajwa <bajwals@live.com>
2020-01-16 20:13:48 -0800

    added dummy data

--- models/schema.sql
+++ models/schema.sql
@@ -18,6 +18,7 @@ CREATE TABLE artwork (
     img_url VARCHAR(100) NULL,
     price INT NULL,
     in_stock INT NULL,
+    buyer_id INT NULL,
     PRIMARY KEY (id)
 );
 
@@ -26,7 +27,16 @@ CREATE TABLE customers (
     firstname VARCHAR(45) NULL,
     lastname VARCHAR(45) NULL,
     username VARCHAR(45) NULL,
-    credit_card INT NULL,
+    credit_card VARCHAR(45) NULL,
     mailing_address VARCHAR(45) NULL,
     PRIMARY KEY (id)
 );
+
+INSERT INTO artists (firstname, lastname, username)
+VALUES ("Lakshdeep", "Bajwa", "bajwals");
+
+INSERT INTO artwork (artist_id, title, img_url, price, in_stock)
+VALUES ("1", "Breathtaking Landscape", "https://puu.sh/F0HaZ/f50c72dd54.jpeg", 100, 1);
+
+INSERT INTO customers (firstname, lastname, username, credit_card, mailing_address)
+VALUES ("Diana", "Maynard", "mokoolaid", "0000", "1 Capitol Mall, Sacramento, CA");
