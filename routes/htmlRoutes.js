4f78cd3c1ba7f17c5b25dd273ae408c5ae87b7ee
Lakshdeep Bajwa <bajwals@live.com>
2020-01-27 19:05:43 -0800

    changed back to res sendFile temporarily

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,8 +4,12 @@ var path = require("path")
 module.exports = function(app) {
   // Load index page
 
+  // app.get("/index", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/styles/index.html"));
+  // });
+
   app.get("/index", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/styles/index.html"));
+    res.render("index");
   });
 
   app.get("/contact", function(req, res) {
@@ -14,6 +18,10 @@ module.exports = function(app) {
 
   app.get("/art", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/styles/art.html"));
+  });
+
+  app.get("/upload", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/styles/upload.html"));
   });
 
   // Render 404 page for any unmatched routes
